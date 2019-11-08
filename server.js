03cba911c83724f883d3f458c40f0dd02dea09c6
Diana Le <lepdiana01@gmail.com>
2019-11-08 00:46:39 -0700

    updated gitignore

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const expressJwt = require('express-jwt')
+const PORT = process.env.PORT || 7000
+const path = require("path")
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/countmanagementdb', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    },
+    () => console.log('Connected to the DB')
+)
+
+app.use('/auth', require('./routes/authRouter.js'))
+app.use('/api', expressJwt({secret: process.env.SECRET}))
+
+app.use('/api/netincome', require('./routes/netIncomeRouter.js'))
+app.use('/api/transaction', require('./routes/transactionRouter.js'))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    if(err.name === 'UnauthorizedError'){
+        res.status(err.status)
+    }
+    return res.send({errMsg: err.message})
+})
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
