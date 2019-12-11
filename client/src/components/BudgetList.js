31dbe61f285bc7788795d629ee25d2a726e6079d
Diana Le <lepdiana01@gmail.com>
2019-12-11 14:41:51 -0700

    added in budgeting template

--- client/src/components/BudgetList.js
+++ client/src/components/BudgetList.js
@@ -1,23 +1,65 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { CrudContext } from '../context/CrudProvider.js'
 
 
 function BudgetList(){
-    const { userTransactions, getUserTransactions, addTransaction, editTransaction, deleteTransaction } = useContext(CrudContext)
+    const { userTransactions, getUserTransactions } = useContext(CrudContext)
 
 
     useEffect(() => {
         getUserTransactions()
     }, [])
 
-    console.log(userTransactions)
+
     return(
         <div>
             <div>
                 <h1>Category</h1>
+                <p>Auto & Transport</p>
+                <p>Bills & Utilities</p>
+                <p>Business Services</p>
+                <p>Education</p>
+                <p>Entertainment</p>
+                <p>Fees & Changes</p>
+                <p>Financial</p>
+                <p>Food & Dining</p>
+                <p>Gifts & Donations</p>
+                <p>Health & Fitness</p>
+                <p>Home</p>
+                <p>Kids</p>
+                <p>Misc Expenses</p>
+                <p>Personal Care</p>
+                <p>Pets</p>
+                <p>Shopping</p>
+                <p>Taxes</p>
+                <p>Transfer</p>
+                <p>Travel</p>
+                <p>Uncategorized</p>
             </div>
             <div>
                 <h1>Budgeted</h1>
+                <form>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                    <input/>
+                </form>
             </div>
             <div>
                 <h1>Activity</h1>
