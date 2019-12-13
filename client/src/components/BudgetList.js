c8ef59e7129604c31d541e7a31a07e9913ced47d
Diana Le <lepdiana01@gmail.com>
2019-12-13 10:50:29 -0700

    setting up the filtered catergory

--- client/src/components/BudgetList.js
+++ client/src/components/BudgetList.js
@@ -4,13 +4,64 @@ import { CrudContext } from '../context/CrudProvider.js'
 
 function BudgetList(){
     const { userTransactions, getUserTransactions } = useContext(CrudContext)
+    const [filteredArr, setFilteredArr] = useState([])
 
 
     useEffect(() => {
         getUserTransactions()
     }, [])
+    
+    
+    const category = userTransactions.forEach(function(transaction) {
+        if (transaction.category) {
+            if (transaction.category === 'Auto & Transport'){
+                
+            } else if (transaction.category === 'Bills & Utilities') {
+                
+            } else if (transaction.category === 'Business Services') {
+                
+            } else if (transaction.category === 'Education') {
+                
+            } else if (transaction.category === 'Entertainment') {
+                
+            } else if (transaction.category === 'Fees & Changes') {
+                
+            } else if (transaction.category === 'Financial') {
+                
+            } else if (transaction.category === 'Food & Dining') {
+                
+            } else if (transaction.category === 'Gifts & Donations') {
+                
+            } else if (transaction.category === 'Health & Fitness') {
+                
+            } else if (transaction.category === 'Home') {
+                
+            } else if (transaction.category === 'Kids') {
+                
+            } else if (transaction.category === 'Misc Expenses') {
+                
+            } else if (transaction.category === 'Personal Care') {
+                
+            } else if (transaction.category === 'Pets') {
+                
+            } else if (transaction.category === 'Shopping') {
+                
+            } else if (transaction.category === 'Taxes') {
+                
+            } else if (transaction.category === 'Transfer') {
+                
+            } else if (transaction.category === 'Travel') {
+                
+            } else if (transaction.category === 'Uncategorized') {
+                
+            }
+        } else {
+            transaction.amount = 0
+        }
+    })
 
-
+    console.log(filteredArr)
+    
     return(
         <div>
             <div>
